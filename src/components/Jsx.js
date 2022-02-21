350cc6dbc999d6e54d1c46a7fdcfe49829a62efa
Gochi <mevere66@gmail.com>
2022-02-21 18:40:39 -0500

    Segunda app en react, utilizando funciones basicas de js

---
+++ src/components/Jsx.js
@@ -0,0 +1,15 @@
+import React, { Fragment } from "react";
+
+const Jsx = () => {
+  //const saludo = "Hola JSX";
+  const temperatura = 21;
+  return (
+    <Fragment>
+      
+      <h2>Frío o calor?</h2>
+      <h4>{temperatura > 20 ? "Calor" : "Frio"}</h4>
+    </Fragment>
+  );
+};
+
+export default Jsx;
